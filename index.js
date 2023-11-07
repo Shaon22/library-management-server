f85cb32e7fa0cefffa9a0a8a8060d6258c1818db
Shaon22 <shaonroy11@gmail.com>
2023-11-08 05:02:01 +0600

    category wise get data successfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -25,12 +25,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const categoryCollection=client.db('library-management-system').collection('booksCategory')
+    const booksCollection=client.db('library-management-system').collection('books')
+    
+    app.get('/books/:category_name',async(req,res)=>{
+      const category_name=req.params.category_name;
+      const query={category : category_name}
+      const result=await booksCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.post('/books',async(req,res)=>{
+      const booksInfo=req.body
+      const result=await booksCollection.insertOne(booksInfo)
+      res.send(result)
+    })
+    
+    app.get('/categories', async (req, res) => {
+      const cursor = categoryCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
