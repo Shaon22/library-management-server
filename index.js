924d05cc71e895740f0d27dd139d7a252a45eb20
Shaon22 <shaonroy11@gmail.com>
2024-01-11 22:28:50 +0600

    something changed

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
+  app.use(cors({
+    origin: ['http://localhost:5173','https://the-atheneum.netlify.app'],
+    credentials:true
+  }))
 app.use(express.json())
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sykxlbw.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sykxlbw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,12 +27,55 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const categoryCollection=client.db('library-management-system').collection('booksCategory')
     const booksCollection=client.db('library-management-system').collection('books')
+    const borrowCollection=client.db('library-management-system').collection('borrowedBooks')
     
-    
+    app.delete('/deleteBooks/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await borrowCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.get('/borrowedBooks/:email',async(req,res)=>{
+      const email=req.params.email;
+      const query={ email: email}
+      const result=await borrowCollection.find(query).toArray()
+      res.send(result)
+    })
+    app.post('/borrowedBooks',async(req,res)=>{
+      const booksInfo=req.body
+      const result=await borrowCollection.insertOne(booksInfo)
+      res.send(result)
+    })
+
+    app.put('/allBooks/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updatedInfo = req.body
+      const books = {
+        $set: {
+          imageURL: updatedInfo.imageURL,
+          name: updatedInfo.name,
+          category: updatedInfo.category,
+          ratings: updatedInfo.ratings,
+          author:updatedInfo.author
+        }
+      }
+      const result = await booksCollection.updateOne(filter, books, options)
+      res.send(result)
+    })
+
+    app.get('/allBooks/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await booksCollection.findOne(query)
+      res.send(result)
+    })
     
     app.get('/allBooks',async(req,res)=>{
       const cursor = booksCollection.find()
@@ -62,13 +108,13 @@ async function run() {
       res.send(result)
     })
     
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+  //   Send a ping to confirm a successful connection
+  //   await client.db("admin").command({ ping: 1 });
+  //   console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+  //   Ensures that the client will close when you finish/error
+  //   await client.close();
+   }
 }
 run().catch(console.dir);
 
